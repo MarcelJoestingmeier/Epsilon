16afb78e33dc5f5da709cfa273e987efefab37d5
Marcel Jöstingmeier <marce_joestingmeier@deichmann.com>
2022-12-12 20:21:40 +0100

    More sophisticated example; Added DI Code; Added Altair

--- Epsilon.Backend/Program.cs
+++ Epsilon.Backend/Program.cs
@@ -1,12 +1,53 @@
+using GraphQL;
+using GraphQL;
+using GraphQL.Caching;
+using GraphQL.MicrosoftDI;
+using GraphQL.SystemTextJson;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using StarWars;
+using GraphQL;
+using GraphQL.MicrosoftDI;
+using GraphQL.Server;
+using GraphQL.SystemTextJson;
+using GraphQL.Types;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using StarWars;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+//builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddGraphQL(b => b
+    .AddSchema<StarWarsSchema>()
+    .AddSystemTextJson()
+    .AddValidationRule<InputValidationRule>()
+    .AddGraphTypes(typeof(StarWarsSchema).Assembly)
+    .AddMemoryCache()
+);
 
+builder.Services.AddSingleton<StarWarsData>();
+builder.Services.AddLogging(builder => builder.AddConsole());
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddControllersWithViews()
+    .AddJsonOptions(opts =>
+    {
+        opts.JsonSerializerOptions.Converters.Add(new InputsJsonConverter());
+    });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,12 +55,15 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseGraphQLAltair();
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.MapControllers();
+// app.MapControllers();
+
+app.UseGraphQL<ISchema>("/graphql");
 
 app.Run();
