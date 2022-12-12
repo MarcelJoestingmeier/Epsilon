5b5e825fa98a54d005d62f0925e57712ce7962c4
Marcel Jöstingmeier <marce_joestingmeier@deichmann.com>
2022-12-12 19:44:24 +0100

    Added GraphQL; GraphQL example type system and query

--- Epsilon.Backend/Controllers/WeatherForecastController.cs
+++ Epsilon.Backend/Controllers/WeatherForecastController.cs
@@ -1,6 +1,34 @@
+using GraphQL;
+using GraphQL.SystemTextJson;
+using GraphQL.Types;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Epsilon.Backend.Controllers;
+
+public class Droid
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+}
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public sealed class DroidType : ObjectGraphType<Droid>
+{
+    public DroidType()
+    {
+        Field(x => x.Id).Description("The Id of the Droid.");
+        Field(x => x.Name).Description("The name of the Droid.");
+    }
+}
+
+public sealed class StarWarsQuery : ObjectGraphType
+{
+    public StarWarsQuery()
+    {
+        Field<DroidType>("hero")
+            .Resolve(context => new Droid { Id = "1", Name = "R2-D2" });
+    }
+}
 
 [ApiController]
 [Route("[controller]")]
@@ -19,14 +47,16 @@ public class WeatherForecastController : ControllerBase
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    public async Task<string> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+        var schema = new Schema { Query = new StarWarsQuery() };
+        
+        var json = await schema.ExecuteAsync(_ =>
+        {
+           _.Query = "{ hero { id name } }";
+        });
+
+        Console.WriteLine(json);
+        return json;
     }
 }
